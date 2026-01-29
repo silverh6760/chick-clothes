597d928baa9c0cf114db97a0bf9318e39f638392
Saleh <hmohamad.sms@gmail.com>
2026-01-30 00:48:20 +0330

    feat: create delivery section

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,7 +1,13 @@
 import CommentSection from "./comment-section/comment-section";
+import DeliverySection from "./delivery-section/delivery-section";
 
 function Home() {
-	return <CommentSection />;
+	return (
+		<div className="flex flex-col gap-5">
+			<DeliverySection />
+			<CommentSection />
+		</div>
+	);
 }
 
 export default Home;
---
+++ src/components/home/delivery-section/delivery-section.tsx
@@ -0,0 +1,11 @@
+function DeliverySection() {
+	return (
+		<div className="w-full bg-[#fbf0ea] p-4 pr-30 pl-30">
+			<div className="flex justify-center items-center font-bold text-[#6c5038] bg-[#f1d1b8] p-3">
+				ارسال رایگان برای خرید بالای چهار میلیون تومان
+			</div>
+		</div>
+	);
+}
+
+export default DeliverySection;
